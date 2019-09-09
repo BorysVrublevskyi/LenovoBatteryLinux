3e6e7b41f3370fef89deaa1fc66814b8187378bf
Borys Vrublevskyi <bvrublevskyi@gmail.com>
2019-09-09 04:33:15 +0300

    Fix default Lenovo G570 safe level (50%)

--- battery_management.sh
+++ battery_management.sh
@@ -6,8 +6,8 @@ if [ "x$(id -u)" != 'x0' ]; then
 fi
 
 function chargeto() {
-if (("$(cat /sys/class/power_supply/BAT1/capacity)" >= "60")); then
-  echo "Battery level higher than 60%"
+if (("$(cat /sys/class/power_supply/BAT1/capacity)" >= "50")); then
+  echo "Battery level higher than 50%"
   exit 1
 else
     echo "Charging battery. Please wait"
@@ -15,7 +15,7 @@ else
     loop=yes
     while [ "$loop" = "yes" ]; do
     sleep 1m
-    if (("$(cat /sys/class/power_supply/BAT1/capacity)" >= "60")); then
+    if (("$(cat /sys/class/power_supply/BAT1/capacity)" >= "50")); then
        echo '\_SB.PCI0.LPCB.EC0.VPC0.SBMC 4' > /proc/acpi/call
        echo "Done, battery charged to $(cat /sys/class/power_supply/BAT1/capacity)%"
        exit 1
@@ -26,7 +26,7 @@ fi
 
 function charger() {
 case "$1" in
-    "60") echo '\_SB.PCI0.LPCB.EC0.VPC0.SBMC 4' > /proc/acpi/call ;;
+    "50") echo '\_SB.PCI0.LPCB.EC0.VPC0.SBMC 4' > /proc/acpi/call ;;
     "100") echo '\_SB.PCI0.LPCB.EC0.VPC0.SBMC 5' > /proc/acpi/call ;;
     "recharge") chargeto ;;
 esac
@@ -36,12 +36,12 @@ echo "Current charge level: $(cat /sys/class/power_supply/BAT1/capacity)%"
 echo ==================================
 echo "1. Enable battery protection mode(Charge level about 55~59%)"
 echo "2. Disable battery protection mode(Charge level 100%)"
-echo "3. Charge battery to 60% and exit"
+echo "3. Charge battery to 50% and exit"
 echo ==================================
 echo -n "Choose an action: "
 read menu
 case "$menu" in
-    1) charger 60 ;;
+    1) charger 50 ;;
     2) charger 100 ;;
     3) charger recharge ;;
 esac
